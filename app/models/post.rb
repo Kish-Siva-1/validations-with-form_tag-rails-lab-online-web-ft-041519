5900988ce82643d54ea70fe119d684500fe01472
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-07-06 10:19:16 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presend: true
-  validates :content, length {is: 100}
+  validates :content, length: {is: 100}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 end

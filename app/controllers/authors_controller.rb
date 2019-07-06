2441d9f5551ddcf710f067f0c03da09f2eef59ab
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-07-06 10:55:11 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -12,7 +12,7 @@ class AuthorsController < ApplicationController
     @author = Author.new(author_params)
 
     if @author.valid?
-      Author.save
+      @author.save
       redirect_to author_path(@author)
     else
       render :new
